f5a9bad5db34227f6aa4f5084b63185048b53daa
Denys.Maruzhenko <Denys.Maruzhenko@uapay.ua>
2020-09-29 13:34:00 +0300

    Changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
             },
             pages: {
                 options: {
-                    remote: 'git@github.com:D-Pushkarevskiy/Gallery/app.git',
+                    remote: 'git@github.com:D-Pushkarevskiy/Gallery.git',
                     branch: 'gh-pages'
                 }
             },
