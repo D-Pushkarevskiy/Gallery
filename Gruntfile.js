51c2134f7e54b98334183d79f0fe7b6bb1fc9fb4
Denys.Maruzhenko <Denys.Maruzhenko@uapay.ua>
2020-09-29 12:14:39 +0300

    Initial version of AngularJS ready to deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-build-control');
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
+        buildcontrol: {
+            options: {
+                dir: 'dist',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages: {
+                options: {
+                    remote: 'git@github.com:<D-Pushkarevskiy>/<Gallery>.git',
+                    branch: 'gh-pages'
+                }
+            },
+            local: {
+                options: {
+                    remote: '../',
+                    branch: 'build'
+                }
+            }
+        },
         concat: {
             stricts: {
                 options: {
